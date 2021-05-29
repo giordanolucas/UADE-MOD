8ed1feb4565eede95bb46818f47eef4ef8d3113e
Gaston Brito <gbrito@gabrito-ltm.internal.salesforce.com>
2021-05-29 00:58:08 -0300

    agrego montecarlo

--- scriptMontecarlo.js
+++ scriptMontecarlo.js
@@ -20,7 +20,73 @@
         // Click en "Calcular" 
         btnCalcular.onclick = function () {
             //Montecarlo
+
+            var funcion = math.parse(document.getElementById("funcion").value);
+            
+            var a = parseFloat(document.getElementById("a").value);
+            var b = parseFloat(document.getElementById("b").value);
+            var n = parseFloat(document.getElementById("N").value);
+            
+            var funcionAnalizada = analizarFuncion(funcion, a, b);
+            var i;
+            var cantPositivos = 0;
+            var cantNegativos = 0;
+
+            var puntosPositivos = { name: 'Puntos Positivos', x: [], y: [], type: 'scatter', mode: "markers" };
+            var puntosNegativos = { name: 'Puntos Negativos', x: [], y: [], type: 'scatter', mode: "markers" };
+
+            for (i = 0; i < n; i++) {
+
+                var xi = math.random(a, b);
+                var yi = math.random(funcionAnalizada.min, funcionAnalizada.max);
+                var fxi = parseFloat(funcion.evaluate({ x: xi }).toString());
+
+                if(yi > 0 && yi <= fxi) {
+                    cantPositivos++;
+                    puntosPositivos.x.push(xi);
+                    puntosPositivos.y.push(yi);
+                } else if (yi < 0 && yi > fxi) {
+                    cantNegativos++;
+                    puntosNegativos.x.push(xi);
+                    puntosNegativos.y.push(yi);
+                }
+            }
+
+            var area = ((cantPositivos - cantNegativos) / n) * (b - a) * (funcionAnalizada.max - funcionAnalizada.min);
+            document.getElementById("totalArea").textContent = "Area Calculada: " + area;
+            Plotly.newPlot('plot', [funcionAnalizada.data, puntosPositivos, puntosNegativos], layout);
         };
     });
 
+    function analizarFuncion(funcion, a, b) {
+        var data = { name: 'f(x)', x: [], y: [], type: 'scatter' };
+        var nk = (b - a) * 100; //TODO Mejorar
+        var min, max;
+        var i;
+
+        for (i = 0; i < nk; i++) {
+          var xi = a + (i * ((b-a)/nk));
+          var fi = parseFloat(funcion.evaluate({ x: xi }).toString());
+
+          data.x.push(xi);
+          data.y.push(fi);
+
+          if(i == 0) {
+            min = fi;
+            max = fi;
+          } else if (fi < min) {
+            min = fi;
+          } else if (fi > max) {
+            max = fi;
+          }
+        }
+
+        return {min: min, max: max, data: data};
+    }
+
+
 })(document, window);
+
+
+
+// n estrella = 50 (es la cant de x chiquitos que tiro)
